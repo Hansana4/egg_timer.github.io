1ce53058892efe58a598e993f7f1bfc1eba7974a
Hansana67676 <83622541+Hansana67676@users.noreply.github.com>
2025-02-20 00:06:48 +1100

    Update app.js

--- app.js
+++ app.js
@@ -20,13 +20,36 @@ function startTimer(startTiming) {
         // When the time runs out
         if (time < 0) {
             clearInterval(interval);
-            count.innerHTML = "DONE!";
-            count.style.right = '24%';
-            
-            backButton.style.display = "block";
+            count.style.display = "none";
+            //count.innerHTML = "DONE!";
+            //count.style.right = '24%';
+            alarm_sound.volume = 1;
+            alarm_sound.play();
+            document.body.style.backgroundImage = "url('finished.gif')";
+            document.body.style.backgroundRepeat = "no-repeat";
+            document.body.style.backgroundSize = "1480px 780px";
         }
     }
-    backButton.addEventListener("click", function() {
-        window.location.href = "index.html";
-    });
+}
+
+// Extract the timer value from the URL query string
+const urlParams = new URLSearchParams(window.location.search);
+const timerValue = urlParams.get('timer') || 6; // Default to 6 minutes if no timer value is found
+
+// Add an event listener to the button
+document.getElementById("startButton").addEventListener("click", function() {
+    startTimer(parseInt(timerValue));
+
+
+});
+
+const alarm_sound = new Audio("capybara_song.mp3"); // Replace with actual audio file
+
+function playSilentAudio() {
+    alarm_sound.volume = 0.00001;
+    alarm_sound.play();
+}
+
+function hide(x){
+    x.style.display = "none";
 }
